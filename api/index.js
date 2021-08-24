a101ca2537358191c260b4dd4d57de91d9ab20af
raloliver <contato@raloliver.com.br>
2021-08-24 09:07:21 +0100

    Add files to project

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const faker = require("faker");
+
+let db = { people: [] };
+const qtty = 12;
+
+for (let index = 0; index <= qtty; index++) {
+    db.people.push({
+        id: index,
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        about: faker.lorem.paragraph(),
+        phone: faker.phone.phoneNumber(),
+        job: faker.name.jobTitle(),
+        image: `https://api.hello-avatar.com/adorables/${faker.internet.email()}`,
+        country: faker.address.country()
+    });
+}
+
+console.log(JSON.stringify(db));
