ed1883ae5c0d5ad7209414920ff3ad1088d1268b
raloliver <contato@raloliver.com.br>
2021-09-15 20:40:11 +0100

    Starting add ui components

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -2,15 +2,18 @@
  * File: shared.module.ts
  * Project: crud-ngrx
  * Created: Wednesday, September 15th 2021, 7:19:36 pm
- * Last Modified: Wednesday, September 15th 2021, 7:42:07 pm
+ * Last Modified: Wednesday, September 15th 2021, 8:32:47 pm
  * Copyright © 2021 AMDE Agência
  */
 
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {TopbarComponent} from './ui/topbar/topbar.component';
+import {MenuComponent} from './ui/menu/menu.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [TopbarComponent, MenuComponent],
     imports: [CommonModule],
+    exports: [TopbarComponent, MenuComponent],
 })
 export class SharedModule {}
---
+++ app/src/app/shared/ui/menu/menu.component.ts
@@ -0,0 +1,20 @@
+/*
+ * File: menu.component.ts
+ * Project: crud-ngrx
+ * Created: Wednesday, September 15th 2021, 8:31:58 pm
+ * Last Modified: Wednesday, September 15th 2021, 8:32:09 pm
+ * Copyright © 2021 AMDE Agência
+ */
+
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.scss'],
+})
+export class MenuComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
