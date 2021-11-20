900e2821f1ca27f2f781052b3fb49dd22ecef492
israelloliveira <Israel.Oliveira@wolterskluwer.com>
2021-11-20 12:07:09 +0000

    feat: add router link on menu component

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@
  * File: shared.module.ts
  * Project: crud-ngrx
  * Created: Wednesday, September 15th 2021, 7:19:36 pm
- * Last Modified: Wednesday, September 15th 2021, 8:32:47 pm
+ * Last Modified: Saturday, November 20th 2021, 12:03:45 pm
  * Copyright © 2021 AMDE Agência
  */
 
@@ -10,10 +10,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TopbarComponent} from './ui/topbar/topbar.component';
 import {MenuComponent} from './ui/menu/menu.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
     declarations: [TopbarComponent, MenuComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     exports: [TopbarComponent, MenuComponent],
 })
 export class SharedModule {}
