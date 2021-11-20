c671c451cc8d4157b07600ef87265cd7ab187eb8
israelloliveira <Israel.Oliveira@wolterskluwer.com>
2021-11-20 12:40:40 +0000

    chore: fixed route path empty to redirect to dashboard

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@
  * File: app-routing.module.ts
  * Project: crud-ngrx
  * Created: Wednesday, September 15th 2021, 6:52:04 pm
- * Last Modified: Wednesday, September 15th 2021, 8:06:26 pm
+ * Last Modified: Saturday, November 20th 2021, 12:39:33 pm
  * Copyright © 2021 AMDE Agência
  */
 
@@ -10,7 +10,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    {path: '**', redirectTo: 'dashboard', pathMatch: 'full'},
     {
         path: 'dashboard',
         loadChildren: () =>
