13d851d5d1c7742953792143dd4e342823ad8e87
raloliver <contato@raloliver.com.br>
2022-01-06 21:17:20 +0000

    feat: category edit form

--- app/src/app/categories/categories.module.ts
+++ app/src/app/categories/categories.module.ts
@@ -2,7 +2,7 @@
  * File: categories.module.ts
  * Project: crud-ngrx
  * Created: Thursday, January 6th 2022, 6:50:47 pm
- * Last Modified: Thursday, January 6th 2022, 8:31:03 pm
+ * Last Modified: Thursday, January 6th 2022, 9:11:34 pm
  * Copyright © 2021 AMDE Agência
  */
 
@@ -12,9 +12,10 @@ import {CommonModule} from '@angular/common';
 import {SharedModule} from '../shared/shared.module';
 import {CategoriesRoutingModule} from './categories-routing.module';
 import {CategoriesPageComponent} from './container/categories-page/categories-page.component';
+import {CategoryEditPageComponent} from './container/category-edit-page/category-edit-page.component';
 
 @NgModule({
-    declarations: [CategoriesPageComponent],
+    declarations: [CategoriesPageComponent, CategoryEditPageComponent],
     imports: [CommonModule, SharedModule, CategoriesRoutingModule],
 })
 export class CategoriesModule {}
