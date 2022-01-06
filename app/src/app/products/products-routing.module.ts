2fc91ccd85cc2aa2a9618c430bda9fd78d6ae8ba
raloliver <contato@raloliver.com.br>
2022-01-06 21:09:24 +0000

    feat: product page and edit form plus links

--- app/src/app/products/products-routing.module.ts
+++ app/src/app/products/products-routing.module.ts
@@ -2,19 +2,29 @@
  * File: products-routing.module.ts
  * Project: crud-ngrx
  * Created: Wednesday, September 15th 2021, 8:16:35 pm
- * Last Modified: Thursday, January 6th 2022, 8:32:42 pm
+ * Last Modified: Thursday, January 6th 2022, 8:43:33 pm
  * Copyright © 2021 AMDE Agência
  */
 
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
+import {ProductEditPageComponent} from './container/product-edit-page/product-edit-page.component';
+import {ProductPageComponent} from './container/product-page/product-page.component';
 import {ProductsPageComponent} from './container/products-page/products-page.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: ProductsPageComponent,
+    },
+    {
+        path: ':id',
+        component: ProductPageComponent,
+    },
+    {
+        path: ':id/edit',
+        component: ProductEditPageComponent,
     },
 ];
 
